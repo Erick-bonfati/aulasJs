d47e4fd592dc00868197d610b4f74c448426a1f8
Erick bonfati <erickbonfati39@gmail.com>
2024-01-09 11:20:31 -0300

    version 13

--- projetoagenda/frontend/modules/Contato.js
+++
@@ -1,7 +0,0 @@
-import validator from "validator";
-
-export default class Contato {
-    constructor(formClass) {
-
-    }
-}
--- projetoagenda/src/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-const Contato = require('../models/ContatoModel');
-
-// Injetando os contatos criados dentro do Index
-exports.index = async(req, res) => {
-    const contatos = await Contato.buscaContatos();
-    res.render('index', { contatos });
-};
